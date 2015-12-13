f62963a1c6a4c4742fb6eab3647b0c80696b4865
Sean Mulligan <sean.mulligan85@gmail.com>
2015-12-13 11:46:22 -0800

    Add ajax call unit test

--- spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_controller_spec.js
@@ -3,12 +3,58 @@ describe("CustomerSearchController", function() {
     var scope = null,
         controller = null;
     beforeEach(module("customers"));
-    beforeEach(inject(function ($controller, $rootScope) {
+    
+    beforeEach(inject(function($controller, $rootScope) {
       scope = $rootScope.$new();
-      controller = $controller("CustomerSearchController", { $scope: scope });
+      controller = $controller("CustomerSearchController", {
+        $scope: scope
+      });
+
     }));
     it("defaults to an empty customer list", function() {
       expect(scope.customers).toEqualData([]);
+    });
+  });
+
+  describe("Fetching Search Results", function() {
+    var scope = null,
+        controller = null,
+        httpBackend = null,
+        serverResults = [
+          {
+            id: 123,
+            first_name: "Bob",
+            last_name: "Jones",
+            email: "bjones@foo.net",
+            username: "jonesy"
+          },
+          {
+            id: 456,
+            first_name: "Bob",
+            last_name: "Johnson",
+            email: "johnboy@bar.info",
+            username: "bobbyj"
+          }
+    ];
+
+    beforeEach(module("customers"));
+    beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+      scope = $rootScope.$new();
+      httpBackend = $httpBackend;
+      controller = $controller("CustomerSearchController", {
+        $scope: scope
+      });
+
+    }));
+    
+    beforeEach(function() {
+      httpBackend.when('GET', '/customers.json?keywords=bob&page=0').respond(serverResults);
+    });
+
+    it("populates the customer list with results", function() {
+      scope.search("bob");
+      httpBackend.flush();
+      expect(scope.customers).toEqualData(serverResults);
     });
   });
 });
