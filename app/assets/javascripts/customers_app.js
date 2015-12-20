78f3aa15ffc2b48b831cddaf259d5af33c39de8d
Sean Mulligan <sean.mulligan85@gmail.com>
2015-12-19 16:30:33 -0800

    Add CustomerDetailController

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,37 +1,55 @@
-var app = angular.module('customers', ['ngRoute', 'templates']);
+var app = angular.module(
+  'customers',
+  [
+    'ngRoute',
+    'templates'
+  ]
+); 
 
 app.config([
-    "$routeProvider", 
-    function($routeProvider) {
-      $routeProvider.when("/", {
-        controller: "CustomerSearchController",
-        templateUrl: "customer_search.html"
-      });
-    }
-    ]);
+          "$routeProvider",
+  function($routeProvider) {
+    $routeProvider.when("/", {
+       controller: "CustomerSearchController",
+      templateUrl: "customer_search.html"
+    }).when("/:id",{
+       controller: "CustomerDetailController",
+      templateUrl: "customer_detail.html"
+    });
+  }
+]);
 
-app.controller("CustomerSearchController", [
-          '$scope','$http',
-  function($scope , $http) {
+app.controller("CustomerSearchController", [ 
+          '$scope','$http','$location',
+  function($scope , $http , $location) {                         
+
+   // rest of controller....
+
+
     var page = 0;
+
     $scope.customers = [];
-    $scope.search = function(searchTerm) {
-      if (searchTerm > 3) {
+    $scope.search = function(searchTerm) {   
+      $scope.loading = true;
+      if (searchTerm.length < 3) {
         return;
       }
-      $http.get("/customers.json",
-                { "params": { "keywords": searchTerm, "page": page  } }
+      $http.get("/customers.json",  
+                { "params": { "keywords": searchTerm, "page": page } }
       ).success(
-        function(data,status,headers,config) {
+        function(data,status,headers,config) { 
           $scope.customers = data;
+          $scope.loading = false;
       }).error(
         function(data,status,headers,config) {
+          $scope.loading = false;
           alert("There was a problem: " + status);
         });
     }
+
     $scope.previousPage = function() {
       if (page > 0) {
-        page = page -1;
+        page = page - 1;
         $scope.search($scope.keywords);
       }
     }
@@ -39,5 +57,28 @@ app.controller("CustomerSearchController", [
       page = page + 1;
       $scope.search($scope.keywords);
     }
+
+    $scope.viewDetails = function(customer) {
+      $location.path("/" + customer.id);
+    }
   }
 ]);
+app.controller("CustomerDetailController", [ 
+          "$scope","$http","$routeParams",
+  function($scope , $http , $routeParams) {
+
+    // Make the AJAX call and set $scope.customer...
+
+    var customerId = $routeParams.id;
+    $scope.customer = {};
+
+    $http.get(
+      "/customers/" + customerId + ".json"
+    ).success(function(data,status,headers,config) {
+      $scope.customer = data;
+    }).error(function(data,status,headers,config) {
+      alert("There was a problem: " + status);
+    });
+  }
+]);
+
